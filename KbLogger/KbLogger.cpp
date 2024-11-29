2e42f95dba47ec89115e7f01d17aea10aac41d2c
rdavini <amerafa@gmail.com>
2024-11-29 12:05:47 -0800

    refactor

--- KbLogger/KbLogger.cpp
+++ KbLogger/KbLogger.cpp
@@ -9,18 +9,20 @@ extern POBJECT_TYPE* IoDriverObjectType;
 
 typedef bool BOOL;
 
-#define KBD_DEVICE 0
-#define MOU_DEVICE 1
+#define KBD_DEVICE '0'
+#define MOU_DEVICE '1'
+#define START_ACTION '2'
+#define END_ACTION '3'
 
 typedef struct {
-	PDEVICE_OBJECT lowerKbdDev;
+	PDEVICE_OBJECT lowerDev;
 	LIST_ENTRY queueHead;
 	KSPIN_LOCK queueLock;
 	HANDLE pFileHandle;
 	PETHREAD pThread;
 	KSEMAPHORE semQueue;
 	BOOL threadTerminated;
-	ULONG deviceType;
+	CHAR deviceType;
 } DEVICE_EXTENSION, *PDEVICE_EXTENSION;
 
 typedef struct {
@@ -35,7 +37,7 @@ typedef struct {
 
 typedef struct {
 	CHAR separator;
-	ULONG delay;
+	LARGE_INTEGER time;
 } SEPARATOR;
 
 extern "C"
@@ -56,20 +58,22 @@ NTSTATUS DispatchRead(PDEVICE_OBJECT DevObj, PIRP irp);
 NTSTATUS ReadComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context);
 void SleepKernel(ULONG milliseconds);
 VOID w2f(PVOID StartContext);
-NTSTATUS CreateDevice(PDRIVER_OBJECT DrvObj, PDEVICE_OBJECT* devObj, unsigned int DeviceType);
-NTSTATUS InitDevice(PDRIVER_OBJECT DrvObj, unsigned int DeviceType);
+NTSTATUS CreateDevice(PDRIVER_OBJECT DrvObj, CHAR DeviceType, PHANDLE hFile);
 VOID printIrpContent(SEPARATOR sep, PMOUSE_INPUT_DATA irpContent);
 VOID printIrpContent(SEPARATOR sep, PKEYBOARD_INPUT_DATA irpContent);
-ULONG getTimeDiff(PLARGE_INTEGER startTime, PLARGE_INTEGER endTime);
 
 BOOL fW2f;
 ULONG key_pending;
-LARGE_INTEGER tLastInput;
 
 extern "C"
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 	UNREFERENCED_PARAMETER(RegistryPath);
 	NTSTATUS status = STATUS_SUCCESS;
+	IO_STATUS_BLOCK fIoStatusBlock;
+	OBJECT_ATTRIBUTES fObjAttr;
+	UNICODE_STRING fName = RTL_CONSTANT_STRING(L"\\??\\C:\\Users\\rdavini\\Documents\\ScreenRec\\kbdMouLog.txt");
+	HANDLE hFile;
+
 	fW2f = false;
 	key_pending = 0;
 
@@ -79,56 +83,53 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) 
 
 	DriverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;
 	DriverObject->DriverUnload = DriverUnloadRoutine;
-	
-	KeQuerySystemTimePrecise(&tLastInput);
 
-	InitDevice(DriverObject, KBD_DEVICE);
-	InitDevice(DriverObject, MOU_DEVICE);
+	InitializeObjectAttributes(&fObjAttr, &fName, OBJ_CASE_INSENSITIVE, NULL, NULL);
 
-	return status;
-}
-
-NTSTATUS CreateDevice(PDRIVER_OBJECT DrvObj, PDEVICE_OBJECT * devObj, unsigned int DeviceType) {
-	ULONG fDeviceType = (DeviceType == KBD_DEVICE ? FILE_DEVICE_KEYBOARD : FILE_DEVICE_MOUSE);
-	NTSTATUS status = IoCreateDevice(DrvObj, sizeof(DEVICE_EXTENSION), NULL, fDeviceType, 0, true, devObj);
+	status = ZwCreateFile(&hFile, FILE_WRITE_DATA, &fObjAttr, &fIoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_WRITE, FILE_SUPERSEDE, FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
 	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create device\n"));
+		KdPrint(("Failed to open file\n"));
 		return status;
 	}
-	(*devObj)->Flags |= DO_BUFFERED_IO;
-	(*devObj)->Flags &= ~DO_DEVICE_INITIALIZING;
+
+	CreateDevice(DriverObject, KBD_DEVICE, &hFile);
+	CreateDevice(DriverObject, MOU_DEVICE, &hFile);
 
 	return status;
 }
 
-NTSTATUS InitDevice(PDRIVER_OBJECT DrvObj, unsigned int DeviceType) {
+NTSTATUS CreateDevice(PDRIVER_OBJECT DrvObj, CHAR DeviceType, PHANDLE hFile) {
 	NTSTATUS status = STATUS_SUCCESS;
-	UNICODE_STRING fName = RTL_CONSTANT_STRING(L"\\??\\C:\\Users\\rdavini\\Documents\\kbdMouLogger.txt");
 	PDEVICE_EXTENSION pDevExt = NULL;
 	PDEVICE_OBJECT devObj;
+	UNICODE_STRING drvPath;
 
-	//refactor use only one variable to hold MOU\KBD path
-	UNICODE_STRING mouPath = RTL_CONSTANT_STRING(L"\\Driver\\MouClass");
-	UNICODE_STRING kbdPath = RTL_CONSTANT_STRING(L"\\Driver\\KbdClass");		
-		
-	PDRIVER_OBJECT mouDriver;
+	DeviceType == KBD_DEVICE ? drvPath = RTL_CONSTANT_STRING(L"\\Driver\\KbdClass") : drvPath = RTL_CONSTANT_STRING(L"\\Driver\\MouClass");
 
-	if (!DeviceType) {
-		status = ObReferenceObjectByName(&kbdPath, 0, NULL, (ACCESS_MASK)0, *IoDriverObjectType, KernelMode, NULL, (PVOID*)&mouDriver);
-	}
-	else {
-		status = ObReferenceObjectByName(&mouPath, 0, NULL, (ACCESS_MASK)0, *IoDriverObjectType, KernelMode, NULL, (PVOID*)&mouDriver);
+	PDRIVER_OBJECT driver;
+
+	status = ObReferenceObjectByName(&drvPath, 0, NULL, (ACCESS_MASK)0, *IoDriverObjectType, KernelMode, NULL, (PVOID*)&driver);
+	if (!NT_SUCCESS(status)) {
+		DbgPrint("Failed to get pointer to driver obj");
 	}
 
-	PDEVICE_OBJECT currentDev = mouDriver->DeviceObject;
+	PDEVICE_OBJECT currentDev = driver->DeviceObject;
+	ULONG fDeviceType = (DeviceType == KBD_DEVICE ? FILE_DEVICE_KEYBOARD : FILE_DEVICE_MOUSE);
 
 	while (currentDev) {
-		status = CreateDevice(DrvObj, &devObj, DeviceType);
+		DbgPrint("Device type: %s current dev %x\n",DeviceType == KBD_DEVICE ? "KEYBOARD" : "MOUSE", currentDev);
+		status = IoCreateDevice(DrvObj, sizeof(DEVICE_EXTENSION), NULL, fDeviceType, 0, true, &devObj);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("Failed to create device\n"));
+			return status;
+		}
+		devObj->Flags |= DO_BUFFERED_IO;
+		devObj->Flags &= ~DO_DEVICE_INITIALIZING;
 
 		pDevExt = (PDEVICE_EXTENSION)devObj->DeviceExtension;
 		RtlZeroMemory(pDevExt, sizeof(DEVICE_EXTENSION));
 
-		pDevExt->lowerKbdDev = IoAttachDeviceToDeviceStack(devObj, currentDev);
+		pDevExt->lowerDev = IoAttachDeviceToDeviceStack(devObj, currentDev);
 
 		if (!NT_SUCCESS(status)) {
 			KdPrint(("Failed to attach device\n"));
@@ -139,20 +140,11 @@ NTSTATUS InitDevice(PDRIVER_OBJECT DrvObj, unsigned int DeviceType) {
 			pDevExt->deviceType = DeviceType;
 			KdPrint(("deviceType: %d\n", DeviceType));
 			pDevExt->threadTerminated = false;
+			pDevExt->pFileHandle = *hFile;
 			InitializeListHead(&pDevExt->queueHead);
 			KeInitializeSpinLock(&pDevExt->queueLock);
 			KeInitializeSemaphore(&pDevExt->semQueue, 0, MAXLONG);
-		}
-
-		OBJECT_ATTRIBUTES fObjAttr;
-		IO_STATUS_BLOCK fIoStatusBlock;
-		InitializeObjectAttributes(&fObjAttr, &fName, OBJ_CASE_INSENSITIVE, NULL, NULL);
-
-		status = ZwOpenFile(&pDevExt->pFileHandle, FILE_APPEND_DATA, &fObjAttr, &fIoStatusBlock, FILE_SHARE_WRITE, FILE_SYNCHRONOUS_IO_NONALERT);
-		if (!NT_SUCCESS(status)) {
-			KdPrint(("Failed to open file\n"));
-			return status;
-		}
+		}		
 
 		HANDLE hThread;
 		status = PsCreateSystemThread(&hThread, (ACCESS_MASK)0, NULL, (HANDLE)0, NULL, w2f, pDevExt);
@@ -172,67 +164,76 @@ NTSTATUS InitDevice(PDRIVER_OBJECT DrvObj, unsigned int DeviceType) {
 		currentDev = currentDev->NextDevice;
 	}
 
-	return status;
-}
+	ObDereferenceObject(driver);
 
-ULONG getTimeDiff(PLARGE_INTEGER startTime, PLARGE_INTEGER endTime) {
-	return (endTime->LowPart - startTime->LowPart) / 10000;
+	return status;
 }
 
 VOID w2f(PVOID StartContext) {
 	UNREFERENCED_PARAMETER(StartContext);
 	PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)StartContext;
+	SEPARATOR sep = { 0 };
+	PLIST_ENTRY entry2 = NULL;
 	IO_STATUS_BLOCK fIoStatusBlock;
-	NTSTATUS status = STATUS_SUCCESS;
-	LARGE_INTEGER tInput;
-	SEPARATOR sep;
-	ULONG timeDiff;
 
-	while (true) {		
+	DbgPrint("Started w2f");
+		
+	sep.separator = pDevExt->deviceType;
+	while (!pDevExt->threadTerminated) {
 		KeWaitForSingleObject(&pDevExt->semQueue, Executive, KernelMode, FALSE, NULL);
 
 		if (pDevExt->threadTerminated) {
 			KdPrint(("Terminating thread %d\n", pDevExt->deviceType));
 			break;
 		}
-		KeQuerySystemTimePrecise(&tInput);
 
-		// Print the difference in ticks
-		timeDiff = getTimeDiff(&tLastInput, &tInput);
-		KdPrint(("Time Difference (millesec): %ul\n", timeDiff));
+		entry2 = ExInterlockedRemoveHeadList(&pDevExt->queueHead, &pDevExt->queueLock);
 
-		//CalculateTimeDifference(tLastInput, tInput);
-		tLastInput = tInput;		
-		
-		sep.delay = timeDiff;
-		sep.separator = pDevExt->deviceType == KBD_DEVICE ? '0' : '1';
+		if (entry2) {
+			KeQuerySystemTimePrecise(&sep.time);
+			if (pDevExt->deviceType == KBD_DEVICE) {
+				KEY_DATA* kData = CONTAINING_RECORD(entry2, KEY_DATA, entry);
+				
+				//the user needs to press f3 to write inputs to file
+				if (kData->key.MakeCode == 0x003D && kData->key.Flags) {
+					fW2f = !fW2f;
 
-		PLIST_ENTRY entry2 = ExInterlockedRemoveHeadList(&pDevExt->queueHead, &pDevExt->queueLock);
-		ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &sep, sizeof(SEPARATOR), 0, 0);
-		if (pDevExt->deviceType == KBD_DEVICE) {
-			KEY_DATA* kData = CONTAINING_RECORD(entry2, KEY_DATA, entry);
-			KdPrint(("W2f keyboard\n"));
-			printIrpContent(sep, &kData->key);
-			ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &kData->key, sizeof(KEYBOARD_INPUT_DATA), 0, 0);
-		}
-		else if (pDevExt->deviceType == MOU_DEVICE) {
-			sep.separator = '1';
-			MOU_DATA * mData = CONTAINING_RECORD(entry2, MOU_DATA, entry);
-			KdPrint(("W2f mouse\n"));
-			printIrpContent(sep, &mData->mouse_input);
-			status = ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &mData->mouse_input, sizeof(MOUSE_INPUT_DATA), 0, 0);
-			KdPrint(("status: %x\n", status));
+					sep.separator = fW2f ? START_ACTION : END_ACTION;
+					DbgPrint("start writing file");
+					ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &sep, sizeof(SEPARATOR), 0, 0);
+					sep.separator = pDevExt->deviceType;
+					
+					DbgPrint(fW2f ? "on\n" : "off\n");
+				}
+
+				if (kData->key.MakeCode != 0x003D && fW2f) {
+					printIrpContent(sep, &kData->key);
+					DbgPrint("writing kbd");
+					ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &sep, sizeof(SEPARATOR), 0, 0);
+					ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &kData->key, sizeof(KEYBOARD_INPUT_DATA), 0, 0);
+				}
+			}
+			else if (pDevExt->deviceType == MOU_DEVICE) {
+				MOU_DATA * mData = CONTAINING_RECORD(entry2, MOU_DATA, entry);
+				
+				if (fW2f) {
+					DbgPrint("writing mouse");
+					printIrpContent(sep, &mData->mouse_input);
+					ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &sep, sizeof(SEPARATOR), 0, 0);
+					ZwWriteFile(pDevExt->pFileHandle, NULL, NULL, NULL, &fIoStatusBlock, &mData->mouse_input, sizeof(MOUSE_INPUT_DATA), 0, 0);
+				}
+			}
 		}
 	}
 	PsTerminateSystemThread(STATUS_SUCCESS);
 }
 
 VOID printIrpContent(SEPARATOR sep, PMOUSE_INPUT_DATA irpContent) {
-	KdPrint(("Sep: sep: %c, delay: %ul \t MouInput unitid:%x \t flags:%x \t rawButtons:%x \t buttons:%d \t Button buttonData: %d \t lastX: %x \t lastY: %x \t extraInformation: %x", sep.separator, sep.delay, irpContent->UnitId, irpContent->Flags, irpContent->RawButtons, irpContent->Buttons, irpContent->ButtonData, irpContent->LastX, irpContent->LastY, irpContent->ExtraInformation));
+	DbgPrint("sep: %c, time: %ul \t MouInput: \n\t { \n\t\t unitid:%x \n\t\t flags:%x \n\t\t rawButtons:%x \n\t\t buttons:%d \n\t\t Button flags: %d \n\t\t buttonData: %d \n\t\t lastX: %x \n\t\t lastY: %x \n\t\t extraInformation: %x\n", sep.separator, sep.time, irpContent->UnitId, irpContent->Flags, irpContent->RawButtons, irpContent->Buttons, irpContent->ButtonFlags, irpContent->ButtonData, irpContent->LastX, irpContent->LastY, irpContent->ExtraInformation);
 }
 
 VOID printIrpContent(SEPARATOR sep, PKEYBOARD_INPUT_DATA irpContent) {
-	KdPrint(("Sep: sep: %c, delay: %ul \tKbdInput unitid:%x \t flags:%x \t makeCode:%x \t reserved: %x \t extraInformation: %x", sep.separator, sep.delay, irpContent->UnitId , irpContent->Flags, irpContent->MakeCode , irpContent->Reserved, irpContent->ExtraInformation));
+	DbgPrint("sep: %c, time: %ul \t KbdInput: \n\t { \n\t\t unitid:%x \n\t\t flags:%x \n\t\t makeCode:%x \n\t\t reserved: %x \n\t\t extraInformation: %x \n", sep.separator, sep.time, irpContent->UnitId , irpContent->Flags, irpContent->MakeCode , irpContent->Reserved, irpContent->ExtraInformation);
 }
 
 void SleepKernel(ULONG milliseconds)
@@ -248,11 +249,11 @@ void SleepKernel(ULONG milliseconds)
 
 void DriverUnloadRoutine(PDRIVER_OBJECT DriverObject) {
 	PDEVICE_OBJECT dev = DriverObject->DeviceObject;
+	PDEVICE_EXTENSION pDevExt;
 	while (dev) {
-		PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)dev->DeviceExtension;
-
+		pDevExt = (PDEVICE_EXTENSION)dev->DeviceExtension;
 		pDevExt->threadTerminated = true;
-		IoDetachDevice(pDevExt->lowerKbdDev);
+		IoDetachDevice(pDevExt->lowerDev);
 		dev = dev->NextDevice;
 	}
 	
@@ -263,7 +264,7 @@ void DriverUnloadRoutine(PDRIVER_OBJECT DriverObject) {
 
 	dev = DriverObject->DeviceObject;
 	while(dev){
-		PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)dev->DeviceExtension;
+		pDevExt = (PDEVICE_EXTENSION)dev->DeviceExtension;
 		ZwClose(pDevExt->pFileHandle);
 		ObDereferenceObject(pDevExt->pThread);
 
@@ -276,7 +277,9 @@ NTSTATUS DriverDispatchRoutine(PDEVICE_OBJECT DevObj, PIRP irp) {
 	PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)DevObj->DeviceExtension;
 
 	IoSkipCurrentIrpStackLocation(irp);
-	return IoCallDriver(pDevExt->lowerKbdDev, irp);
+
+
+	return IoCallDriver(pDevExt->lowerDev, irp);
 }
 
 NTSTATUS DispatchRead(PDEVICE_OBJECT DevObj, PIRP irp) {
@@ -288,7 +291,7 @@ NTSTATUS DispatchRead(PDEVICE_OBJECT DevObj, PIRP irp) {
 	
 	key_pending++;
 
-	return IoCallDriver(pDevExt->lowerKbdDev, irp);
+	return IoCallDriver(pDevExt->lowerDev, irp);
 }
 
 NTSTATUS ReadComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context) {
@@ -297,30 +300,16 @@ NTSTATUS ReadComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context) {
 
 	PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)Context;
 
-	KdPrint(("Read Complete\n"));
-	KdPrint(("device: %d\n", pDevExt->deviceType));
 	if (Irp->IoStatus.Status == STATUS_SUCCESS) {
-		KdPrint(("key_pending: %d\n", key_pending));
-		if (!pDevExt->deviceType) {
+		if (pDevExt->deviceType == KBD_DEVICE) {
 			PKEYBOARD_INPUT_DATA input = (PKEYBOARD_INPUT_DATA)Irp->AssociatedIrp.SystemBuffer;
-
-			if (input->MakeCode == 0x003D && input->Flags) {
-				fW2f = !fW2f;
-				KdPrint((fW2f ? "on\n" : "off\n"));
-			}
-
-			KdPrint(("input: %d \n", input->MakeCode));
-
 			KEY_DATA* entry = (KEY_DATA*)ExAllocatePoolWithTag(NonPagedPool, sizeof(KEY_DATA), 'ktag');
 			RtlZeroMemory(entry, sizeof(KEY_DATA));
 
 			entry->key = *input;
 
-			if (fW2f && input->MakeCode != 0x003D) {
-				ExInterlockedInsertTailList(&pDevExt->queueHead, &entry->entry, &pDevExt->queueLock);
-				KeReleaseSemaphore(&pDevExt->semQueue, 0, 1, FALSE);
-			}
-			ExFreePoolWithTag(entry, 'ktag');
+			ExInterlockedInsertTailList(&pDevExt->queueHead, &entry->entry, &pDevExt->queueLock);
+			KeReleaseSemaphore(&pDevExt->semQueue, 0, 1, FALSE);
 		}
 		else {
 			PMOUSE_INPUT_DATA input = (PMOUSE_INPUT_DATA)Irp->AssociatedIrp.SystemBuffer;
@@ -329,55 +318,8 @@ NTSTATUS ReadComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context) {
 			
 			entry->mouse_input = *input;
 			
-			KdPrint(("\nMouInput unitid:%x \t flags:%x \t rawButtons:%x \t buttons:%d \t Button buttonData: %d \t lastX: %x \t lastY: %x \t extraInformation: %x", entry->mouse_input.UnitId, entry->mouse_input.Flags, entry->mouse_input.RawButtons, entry->mouse_input.Buttons, entry->mouse_input.ButtonData, entry->mouse_input.LastX, entry->mouse_input.LastY, entry->mouse_input.ExtraInformation));
-			
-			switch (entry->mouse_input.ButtonFlags) {
-				case MOUSE_LEFT_BUTTON_DOWN:
-					KdPrint(("Button buttonFlags: MOUSE_LEFT_BUTTON_DOWN \t"));
-					break;
-				case MOUSE_LEFT_BUTTON_UP:
-					KdPrint(("Button buttonFlags: MOUSE_LEFT_BUTTON_UP \t"));
-					break;
-				case MOUSE_RIGHT_BUTTON_DOWN:
-					KdPrint(("Button buttonFlags: MOUSE_RIGHT_BUTTON_DOWN \t"));
-					break;
-				case MOUSE_RIGHT_BUTTON_UP:
-					KdPrint(("Button buttonFlags: MOUSE_RIGHT_BUTTON_UP \t"));
-					break;
-				case MOUSE_MIDDLE_BUTTON_DOWN:
-					KdPrint(("Button buttonFlags: MOUSE_MIDDLE_BUTTON_DOWN \t"));
-					break;
-				case MOUSE_MIDDLE_BUTTON_UP:
-					KdPrint(("Button buttonFlags: MOUSE_MIDDLE_BUTTON_UP \t"));
-					break;
-				case MOUSE_BUTTON_4_DOWN:
-					KdPrint(("Button buttonFlags: MOUSE_BUTTON_4_DOWN \t"));
-					break;
-				case MOUSE_BUTTON_4_UP:
-					KdPrint(("Button buttonFlags: MOUSE_BUTTON_4_UP \t"));
-					break;
-				case MOUSE_BUTTON_5_DOWN:
-					KdPrint(("Button buttonFlags: MOUSE_BUTTON_5_DOWN \t"));
-					break;
-				case MOUSE_BUTTON_5_UP:
-					KdPrint(("Button buttonFlags: MOUSE_BUTTON_5_UP \t"));
-					break;
-				case MOUSE_WHEEL:
-					KdPrint(("Button buttonFlags: MOUSE_WHEEL \t"));
-					break;
-				case MOUSE_HWHEEL:
-					KdPrint(("Button buttonFlags: MOUSE_HWHEEL \t"));
-					break;
-				default:
-					KdPrint(("Button buttonFlags: %d not treated \t", entry->mouse_input.ButtonFlags));
-					break;
-			}
-			
-			if (fW2f) {
-				ExInterlockedInsertTailList(&pDevExt->queueHead, &entry->entry, &pDevExt->queueLock);
-				KeReleaseSemaphore(&pDevExt->semQueue, 0, 1, FALSE);
-			}
-			ExFreePoolWithTag(entry, 'mtag');
+			ExInterlockedInsertTailList(&pDevExt->queueHead, &entry->entry, &pDevExt->queueLock);
+			KeReleaseSemaphore(&pDevExt->semQueue, 0, 1, FALSE);			
 		}
 	}
 	
